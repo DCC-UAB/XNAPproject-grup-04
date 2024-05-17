f736dc688989db22db0e6d8e3275166cb6f5ed03
PClavera <pau.clavera03@gmail.com>
2024-05-17 13:56:38 +0000

    Commit2

--- Seq2Seq/model.py
+++ Seq2Seq/model.py
@@ -3,7 +3,8 @@ import torch.nn as nn
 from torch import optim
 import torch.nn.functional as F
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+print(device)
 MAX_LENGTH = 25
 
 class EncoderRNN(nn.Module):
