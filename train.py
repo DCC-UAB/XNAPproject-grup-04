0e62b46404a6c1ded4212e8e3137d2da31c38b22
Mar Blazquez <mar.blazquez@autonoma.cat>
2024-05-27 20:39:28 +0200

    loss epoch

--- train.py
+++ train.py
@@ -109,6 +109,7 @@ def trainIters(encoder, decoder, n_iters, pairs, print_every=1000, plot_every=10
             print_loss_avg = print_loss_total / print_every
             print_loss_total = 0
             print('%s (%d %d%%) %.4f' % (timeSince(start, iter / n_iters), iter, iter / n_iters * 100, print_loss_avg))
+            wandb.log({"Training Loss Avg": print_loss_avg})
 
         if iter % plot_every == 0:
             plot_loss_avg = plot_loss_total / plot_every
