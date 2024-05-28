a0949d9ebeb69410e2c37ad7d207a2236cd0fed8
Laura Marín Bayo <1636713@uab.com>
2024-05-28 20:21:35 +0200

    validation

--- validation.py
+++ validation.py
@@ -7,3 +7,20 @@ import torch.nn.functional as F
 from data_process import *
 from model import *
 
+def evaluate(encoder, decoder, sentence, input_lang, output_lang):
+    with torch.no_grad():
+        input_tensor = tensorFromSentence(input_lang, sentence)
+
+        encoder_outputs, encoder_hidden = encoder(input_tensor)
+        decoder_outputs, decoder_hidden, decoder_attn = decoder(encoder_outputs, encoder_hidden)
+
+        _, topi = decoder_outputs.topk(1)
+        decoded_ids = topi.squeeze()
+
+        decoded_words = []
+        for idx in decoded_ids:
+            if idx.item() == EOS_token:
+                decoded_words.append('<EOS>')
+                break
+            decoded_words.append(output_lang.index2word[idx.item()])
+    return decoded_words, decoder_attn
